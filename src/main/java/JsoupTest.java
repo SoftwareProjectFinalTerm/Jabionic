7fdec0a58717ce9e81fc1bf155f606078526254e
igeonho <iGH01pr@outlook.com>
2022-06-08 18:05:05 +0900

    Jsoup초기설정

---
+++ src/main/java/JsoupTest.java
@@ -0,0 +1,26 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class JsoupTest {
+    public static void main(String[] args) {
+        try{
+            //(네이버 뉴스기사 전용)url입력
+            String url="https://n.news.naver.com/mnews/article/293/0000039337?sid=105";
+            //웹에서 내용 가져오기
+            Document doc= Jsoup.connect(url).get();
+            //내용 중 원하는 부분 가져오기
+            Elements contents=doc.select("#dic_area");
+            //얻어온 element를 String형태로 변환
+            String text=contents.text();
+
+            //기사 본문 내용 출력
+            System.out.println(text);
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
