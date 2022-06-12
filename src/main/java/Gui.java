70c1fdbd72ff620d90b43afbc37323eb538405ce
igeonho <iGH01pr@outlook.com>
2022-06-12 19:09:21 +0900

    -JsoutTest에 classifySite(String url)메소드 추가. 해당 메소드 기능: url을 입력받고 네이버뉴스,다음뉴스냐에 따라서 다른 태그로 크롤링함. 본문 내용 String으로 리턴. 예외 발생시 "error"리턴.
    -JsoutTest에 crawling(String url)메소드 수정.
    classifySite(url)을 통해 얻은 문자열이 "error"일 경우 빈 리스트 리턴. 이를 이용해 예외처리가 필요해 보임.

--- src/main/java/Gui.java
+++ src/main/java/Gui.java
@@ -52,6 +52,6 @@ public class Gui extends JFrame{
     public static void main(String[] args) {
 //		new Gui();
 		JsoupTest jsoupTest = new JsoupTest();
-		jsoupTest.crawling("https://n.news.naver.com/mnews/article/293/0000039337?sid=105");
+		jsoupTest.crawling("https://news.v.daum.net/v/20220612182429601");
     }
 }
--- src/main/java/JsoupTest.java
+++ src/main/java/JsoupTest.java
@@ -3,6 +3,8 @@ import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class JsoupTest {
@@ -17,29 +19,72 @@ public class JsoupTest {
      * @param url
      * @return
      */
-    public List<ResponseCrawling> crawling(String url) {
+
+    //url을 입력받고 네이버뉴스,다음뉴스냐에 따라서 다른 태그로 크롤링함. 본문 내용 String으로 리턴
+    //예외 상황 발생시 "error"를 리턴
+    String classifySite(String url){
         String text = "";
-        try {
-            //웹에서 내용 가져오기
-            Document doc = Jsoup.connect(url).get();
-            //내용 중 원하는 부분 가져오기
-            Elements contents = doc.select("#dic_area");
-            //얻어온 element를 String형태로 변환
-            text = contents.text();
+        //네이버일 경우
+        if(url.contains("news.naver.com")){
+            try {
+                //웹에서 내용 가져오기
+                Document doc = Jsoup.connect(url).get();
+                //내용 중 원하는 부분 가져오기
+                Elements contents = doc.select("#dic_area");
+                //얻어온 element를 String형태로 변환
+                text = contents.text();
+                //기사 본문 내용 출력
+                System.out.println(text);
+                return text;
 
-            //기사 본문 내용 출력
-            System.out.println(text);
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "error";
+            }
+        }
+        //다음일 경우
+        else if(url.contains("news.v.daum.net")){
+            try {
+                //웹에서 내용 가져오기
+                Document doc = Jsoup.connect(url).get();
+                //내용 중 원하는 부분 가져오기
+                Elements contents = doc.select("#harmonyContainer > section");
+                //얻어온 element를 String형태로 변환
+                text = contents.text();
+                //기사 본문 내용 출력
+                System.out.println(text);
+                return text;
 
-        } catch (IOException e) {
-            e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "error";
+            }
+        }
+        else{
+            System.out.println("지원되지 않는 사이트");
+            return "error";
+        }
+    }
+
+    public List<ResponseCrawling> crawling(String url) {
+        Transfer tf=new Transfer();
+        String text = "";
+        text=classifySite(url); //본문 내용 가져옴
+
+        //본문내용이 정상적으로 가져왔을시
+        if(text!="error"){
+            // 결과 확인용 출력 구문
+            for(ResponseCrawling responseCrawling : tf.transfer(text)) {
+                System.out.print(responseCrawling.getResultWord());
+                System.out.println(" / " + responseCrawling.getStressIndex());
+            }
+            return tf.transfer(text);
+        }
+        //url이 잘못되거나 본문 내용 읽을때 오류 발생. 빈 리스트 리턴
+        else{
+            List<ResponseCrawling> emptyList = Collections.emptyList();
+            return emptyList;
         }
 
-        // 결과 확인용 출력 구문
-        for(ResponseCrawling responseCrawling : tf.transfer(text)) {
-            System.out.print(responseCrawling.getResultWord());
-            System.out.println(" / " + responseCrawling.getStressIndex());
-        }
-
-        return tf.transfer(text);
     }
 }
