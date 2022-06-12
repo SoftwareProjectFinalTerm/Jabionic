2d432ee1316836c6a14894fc955048fb0fb87b3d
seung yeon <syon1203@me.com>
2022-06-13 01:59:25 +0900

    gui 수정
    +예시 문장을 추가해서 위치 지정(혹은 출력 방법 수정?)
    사용자에게 링크 입력을 받아 기사 내용 JTextArea에 출력

--- src/main/java/Gui.java
+++ src/main/java/Gui.java
@@ -1,12 +1,19 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.util.List;
 
 public class Gui extends JFrame{
 
+	JsoupTest jsoupTest = new JsoupTest();
 	JTextField link = new JTextField(30);
 	JTextArea Text = new JTextArea(25,40);
+	
 	JScrollPane scrollText = new JScrollPane(Text);
 	JButton readButton = new JButton("Read");
+	String url = "";
 
 	public Gui() {
 		setTitle("Bionic reader");
@@ -30,28 +37,46 @@ public class Gui extends JFrame{
 		setResizable(false);
 	}
 	
+	//바이오닉리딩 결과 띄워주는 패널
 	class NorthPanel extends JPanel { 
 		public void paintComponent(Graphics g) {
+			
+			//고정위치를 나타내는 부분 삭제(상하 줄만 제외 제거함) 
 			super.paintComponent(g);
 			g.drawLine(10,60,490,60); 
-			g.drawLine(200,60,200,70); 
-			g.drawLine(200,160,200,170); 
+			//g.drawLine(200,60,200,70); 
+			//g.drawLine(200,160,200,170); 
 			g.drawLine(10,170,490,170); 
+			g.setFont(new Font("맑은 고딕",Font.PLAIN,30));
+
+			//String str = "example string";
+			//g.drawString(str,100,125);
 		}
 	}
-	
+
+	//링크 받아오기, 기사 전체 출력 패널
 	class SouthPanel extends JPanel {
 		public SouthPanel() {
 			add(scrollText);
 			add(new JLabel("Link"));
 			add(link);
 			add(readButton);
+			
+			//링크 사용자에게 입력받도록 함
+			readButton.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					url = link.getText();
+					//JTextArea에서 기사 내용 볼 수 있도록 수정
+					Text.setLineWrap(true);
+				    Text.setWrapStyleWord(true);
+					Text.setText(jsoupTest.classifySite(url));
+					//jsoupTest.crawling(url);					
+				}
+			});
 		}
 	}
 	
     public static void main(String[] args) {
-//		new Gui();
-		JsoupTest jsoupTest = new JsoupTest();
-		jsoupTest.crawling("https://news.v.daum.net/v/20220612182429601");
+		new Gui();
     }
 }
